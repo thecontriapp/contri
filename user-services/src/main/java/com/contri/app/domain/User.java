f90f4204ad6e007998a89111ebc1bcc9da8afc99
sumitlole1985 <sumit.lole@gmail.com>
2016-03-05 18:25:25 +0530

    Stable Gradle project structure

--- user-services/src/main/java/com/contri/app/domain/User.java
+++ user-services/src/main/java/com/contri/app/domain/User.java
@@ -4,4 +4,8 @@ package com.contri.app.domain;
  * Created by sumitsmart on 05-Mar-16.
  */
 public class User {
+
+    public static void main(String args[]){
+        System.out.println("Hellow World");
+    }
 }
--- user-services/src/test/java/com/contri/app/domain/UserTest.java
+++ user-services/src/test/java/com/contri/app/domain/UserTest.java
@@ -1,7 +1,20 @@
 package com.contri.app.domain;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 /**
  * Created by sumitsmart on 05-Mar-16.
  */
-public class UserTest {
+public class UserTest
+{
+    @Test
+    public void testMethod(){
+
+        //assertEquals("Objects are not equals.",1,1,0);
+        Assert.assertTrue(true);
+
+    }
+
 }
